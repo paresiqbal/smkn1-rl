733ce0575e39c6b94f3d9b154986f6ac289b8f66
paresiqbal <thecrowdel@gmail.com>
2025-03-04 13:08:28 +0700

    feat: add AdminDashboard component and DashboardController; update Home page content

---
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminDashboard() {
+    return (
+        <div>
+            <h1> Wellcome To Admin Dashboard</h1>
+            <p>Where you Manage Content</p>
+        </div>
+    );
+}
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,6 +4,10 @@ export default function Home() {
     return (
         <div>
             <h1>Welcome to Home Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatum, ipsa!
+            </p>
         </div>
     );
 }
