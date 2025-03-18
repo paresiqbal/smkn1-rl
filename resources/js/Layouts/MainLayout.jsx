eb631ae9c9dddc61da55c1850b6940cbf863ce30
paresiqbal <thecrowdel@gmail.com>
2025-03-18 19:41:52 +0700

    feat: add utility functions and configure TypeScript paths; update MainLayout and app styles

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -8,7 +8,7 @@ export default function MainLayout({ children }) {
 
     return (
         <div className="text-base antialiased font-normal transition-all duration-200 flex flex-col w-full">
-            <Navbar authUser={auth.user} />
+            <Navbar />
             <main className="flex-1 flex justify-center p-4 w-full lg:px-24 selection:bg-red-300 dark:selection:bg-orange-400">
                 {children}
             </main>
