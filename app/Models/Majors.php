9e58b44d1e53405b7139700c5a620fbf42389ffd
paresiqbal <thecrowdel@gmail.com>
2025-04-14 21:18:31 +0700

    feat: define relationships in Majors model for subjects, galleries, and careers

--- app/Models/Majors.php
+++ app/Models/Majors.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Majors extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function subjects()
+    {
+        return $this->hasMany(Subjects::class, 'major_id');
+    }
+
+    public function galleries()
+    {
+        return $this->hasMany(Galleries::class, 'major_id');
+    }
+
+    public function careers()
+    {
+        return $this->hasMany(Careers::class, 'major_id');
+    }
 }
