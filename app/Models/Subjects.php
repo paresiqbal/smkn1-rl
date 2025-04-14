a8708307db5cedd3ad6ef4d33644e40585f7e496
paresiqbal <thecrowdel@gmail.com>
2025-04-14 21:19:03 +0700

    feat: add Subjects model with relationships to Majors and factory support

--- app/Models/Subjects.php
+++ app/Models/Subjects.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Subjects extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function major()
+    {
+        return $this->belongsTo(Majors::class, 'major_id');
+    }
 }
