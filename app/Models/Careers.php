850f9fd1781a3effa53c8c34b9d0a09ed586d838
paresiqbal <thecrowdel@gmail.com>
2025-04-14 21:19:40 +0700

    feat: add Careers model with major relationship and factory support

--- app/Models/Careers.php
+++ app/Models/Careers.php
@@ -4,7 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Careers extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function major()
+    {
+        return $this->belongsTo(Majors::class, 'major_id');
+    }
 }
