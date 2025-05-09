9cdddd92928d13d8e0ea151634c06774b2a9badd
paresiqbal <thecrowdel@gmail.com>
2025-05-09 15:15:26 +0700

    Remove unused ShowCalendar component import from CreateCalendar

--- resources/js/Pages/Admin/guide/Calendar.jsx
+++ resources/js/Pages/Admin/guide/Calendar.jsx
@@ -7,7 +7,6 @@ import NotyfContext from "@/context/NotyfContext";
 // Layout & Components
 import AdminLayout from "@/layouts/AdminLayout";
 import Breadcrumb from "@/components/Breadcrumb";
-import ShowCalendar from "./ShowCalendar";
 
 export default function CreateCalendar() {
     const notyf = useContext(NotyfContext);
@@ -127,9 +126,6 @@ export default function CreateCalendar() {
                     {submitting ? "Mengunggah..." : "Upload"}
                 </button>
             </form>
-            <div>
-                <ShowCalendar />
-            </div>
         </div>
     );
 }
